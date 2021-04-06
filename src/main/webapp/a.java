aea869871660c814245ac99e18ff5c083ccc4749
akshitadixit <akshitadixit.hack@gmail.com>
2021-04-06 12:12:00 +0530

    Quick updates

--- src/main/webapp/a.java
+++
@@ -1,7 +0,0 @@
-class a{
-    public static void main(String args[]) {
-        String u = "scbn";
-        System.out.println("insert into trackizer.retailer(username, password, email) values('"+u+"','"+u+"','"+u+"');");
-    }
-    
-}
